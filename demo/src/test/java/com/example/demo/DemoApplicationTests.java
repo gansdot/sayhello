a57ec78e4a97fe2c0bb4fd2917fb4424a9991537
ganeshanm <ganesan@172.24.5.125>
2020-01-30 10:54:04 +0100

    adding junit test case

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import java.net.URL;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DisplayName("HelloApplicationControllerTests")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// bind RANDOM_PORT
+	@LocalServerPort
+	private int port;
+	
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @DisplayName("sayHello Rest point Test")    
+    @Test
+    @Order(1)
+    public void getHello() throws Exception {
+
+        ResponseEntity<String> response = restTemplate.getForEntity(
+			new URL("http://localhost:" + port + "/hello/name").toString(), String.class);
+        assertEquals("Hello name", response.getBody());
+
+    }
 
 }
